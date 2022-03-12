e6270e9738fac8e231faac85957688cbc3aa42c0
katlego <moimakatlego@outlook.com>
2022-03-12 12:51:13 +0200

    setting states

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 
 function App() {
+  const [value, setValue] = useState('');
+  const [copied, setCopied] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <center>
+    <input
+    
+    />
+    </center>
+    </>
   );
 }
 
